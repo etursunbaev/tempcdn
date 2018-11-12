177a4adb846ddb769b5b64e9c044c2bb9090a4e3
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-11-12 16:48:56 +0600

    hmmm

---
+++ server.js
@@ -0,0 +1,39 @@
+/*
+ * Fake CDN
+ */
+
+var express = require("express");
+var compression = require("compression");
+
+var app = express();
+
+app.use(compression());
+
+app.get("/kurjun/rest/raw/id", function(req, res) {
+    var name = req.query["name"];
+    res.type("text/plain");
+    res.sendFile("id/" + name, {root: "./"});
+});
+
+app.get("/kurjun/rest/raw/info", function(req, res) {
+    var name = req.query["name"];
+    res.type("text/plain");
+    res.sendFile("info/" + name, {root: "./"});
+});
+
+app.get("/kurjun/rest/raw/download", function(req, res) {
+    var name = req.query["name"];
+    var id = req.query["id"];
+    
+    if (name) {
+        res.sendFile("download/" + name, {root: "./"});
+    } else {
+        res.sendFile("download/" + id, {root: "./"});   
+    }
+});
+
+app.listen(18338);
+
+/* 
+ * vim: ts=4 et nowrap autoindent
+ */
